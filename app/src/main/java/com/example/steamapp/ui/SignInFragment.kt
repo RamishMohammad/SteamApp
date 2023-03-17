e55ea2ba658f2fe8b3786e8689d90db0066ff693
RamishMohammad <mohammar@oregonstate.edu>
2023-03-16 18:10:12 -0700

    added log in page with steam api

--- app/src/main/java/com/example/steamapp/ui/SignInFragment.kt
+++ app/src/main/java/com/example/steamapp/ui/SignInFragment.kt
@@ -6,6 +6,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.steamapi.api.AuthenticateUserRequest
 import com.example.steamapi.api.SteamAuth
@@ -37,34 +38,39 @@ class SignInFragment : Fragment() {
         steamService = SteamService.create()
 
         binding.signInButton.setOnClickListener {
-            val steamid = binding.usernameEditText.text.toString()
+            val steamid = binding.usernameEditText.text?.toString() ?: ""
             val password = binding.passwordEditText.text.toString()
-
-            // Authenticate user with Steam Web API
-            GlobalScope.launch(Dispatchers.IO) {
-                val request = AuthenticateUserRequest(steamid, password, SteamService.API_KEY)
-
-                val response = steamService.authenticateUser(request)
+            val errorMessage = "An error occurred. Please try again later."
 
 
-                if (response.isSuccessful) {
-                    val steamAuth = response.body()?.response
-                    if (steamAuth != null && steamAuth.success) {
-                        // User is authenticated
-                        Log.d(TAG, "API call succeeded with status code: ${response.code()}")
-                        Log.d(TAG, "Response data: ${response.body()}")
+            if (steamid.isNotEmpty()) {
+                GlobalScope.launch(Dispatchers.IO) {
+                    val request = AuthenticateUserRequest(steamid, password, SteamService.API_KEY)
+                    val response = steamService.authenticateUser(request)
+                    if (response.isSuccessful) {
+                        val steamAuth = response.body()?.response
+                        if (steamAuth != null && steamAuth.success) {
+                            // User is authenticated
+                            Log.d(TAG, "API call succeeded with status code: ${response.code()}")
+                            Log.d(TAG, "Response data: ${response.body()}")
 
+                        } else {
+                            Log.d(TAG, "FAILEDDD")
+                            // TODO: Show an error message to the user
+                        }
                     } else {
-                        Log.d(TAG, "FAILEDDD")
+                        Log.d(TAG, "API FAILEDDD")
+                        Log.e(TAG, response.errorBody()?.string() ?: "Error body is null")
+
+
                         // TODO: Show an error message to the user
                     }
-                } else {
-                    Log.d(TAG, "API FAILEDDD")
-                    Log.e(TAG, response.errorBody()?.string() ?: "Error body is null")
-
-
-                    // TODO: Show an error message to the user
                 }
+            } else {
+                activity?.runOnUiThread {
+                    Toast.makeText(requireContext(), "Error: $errorMessage", Toast.LENGTH_SHORT).show()
+                }
+
             }
         }
     }
