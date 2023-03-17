5c275ceceba4a5a78232313dd243d1823d77a2e4
RamishMohammad <mohammar@oregonstate.edu>
2023-03-16 17:54:26 -0700

    added log in page with steam api

---
+++ app/src/main/java/com/example/steamapp/api/SteamService.kt
@@ -0,0 +1,54 @@
+package com.example.steamapi.api
+
+import com.squareup.moshi.Json
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Query
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.KotlinJsonAdapterFactory
+import retrofit2.http.Body
+import retrofit2.http.POST
+
+
+interface SteamService {
+    @POST("ISteamUserAuth/AuthenticateUser/v0001/")
+    suspend fun authenticateUser(
+        @Body requestBody: AuthenticateUserRequest
+    ) : Response<SteamAuthResponse>
+
+    companion object {
+        private const val BASE_URL = "https://api.steampowered.com/"
+        const val API_KEY = "C5047CA4CB69A58EAD6BC1B6C8D895C7"
+
+        fun create() : SteamService {
+            val moshi = Moshi.Builder()
+                .add(KotlinJsonAdapterFactory())
+                .build()
+            return Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(MoshiConverterFactory.create(moshi))
+                .build()
+                .create(SteamService::class.java)
+        }
+    }
+}
+
+data class AuthenticateUserRequest(
+    @Json(name = "steamid") val steamId: String,
+    @Json(name = "password") val password: String,
+    @Json(name = "key") val apiKey: String
+)
+
+data class SteamAuthResponse(
+    val response: SteamAuth
+)
+
+data class SteamAuth(
+    val steamid: String,
+    val success: Boolean,
+    val ticket: String
+)
+
+
--- app/src/main/java/com/example/steamapp/ui/SignInFragment.kt
+++ app/src/main/java/com/example/steamapp/ui/SignInFragment.kt
@@ -1,38 +1,71 @@
 package com.example.steamapp.ui
 
+import android.content.ContentValues.TAG
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.findNavController
-import com.example.steamapp.R
+import com.example.steamapi.api.AuthenticateUserRequest
+import com.example.steamapi.api.SteamAuth
+import com.example.steamapi.api.SteamService
 import com.example.steamapp.databinding.FragmentSignInBinding
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 
 class SignInFragment : Fragment() {
 
     private var _binding: FragmentSignInBinding? = null
+    private lateinit var steamService: SteamService
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
         _binding = FragmentSignInBinding.inflate(inflater, container, false)
         return binding.root
-
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.buttonFirst.setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        steamService = SteamService.create()
+
+        binding.signInButton.setOnClickListener {
+            val steamid = binding.usernameEditText.text.toString()
+            val password = binding.passwordEditText.text.toString()
+
+            // Authenticate user with Steam Web API
+            GlobalScope.launch(Dispatchers.IO) {
+                val request = AuthenticateUserRequest(steamid, password, SteamService.API_KEY)
+
+                val response = steamService.authenticateUser(request)
+
+
+                if (response.isSuccessful) {
+                    val steamAuth = response.body()?.response
+                    if (steamAuth != null && steamAuth.success) {
+                        // User is authenticated
+                        Log.d(TAG, "API call succeeded with status code: ${response.code()}")
+                        Log.d(TAG, "Response data: ${response.body()}")
+
+                    } else {
+                        Log.d(TAG, "FAILEDDD")
+                        // TODO: Show an error message to the user
+                    }
+                } else {
+                    Log.d(TAG, "API FAILEDDD")
+                    Log.e(TAG, response.errorBody()?.string() ?: "Error body is null")
+
+
+                    // TODO: Show an error message to the user
+                }
+            }
         }
     }
 
