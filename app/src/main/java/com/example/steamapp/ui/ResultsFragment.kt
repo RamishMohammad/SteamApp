04abcd1e21d0bb9b04cb7edbeb8937560e8b4d44
Charlie Say <charliesay1990@hotmail.com>
2023-03-22 02:12:33 -0700

    add popup menu to results item

--- app/src/main/java/com/example/steamapp/ui/ResultsFragment.kt
+++ app/src/main/java/com/example/steamapp/ui/ResultsFragment.kt
@@ -2,10 +2,10 @@ package com.example.steamapp.ui
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import android.widget.PopupMenu
 import android.widget.Toast
+import androidx.core.view.get
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -75,18 +75,39 @@ class ResultsFragment : Fragment() {
             override fun onItemClick(position: Int) {
                 val newPosition = position + 1
                 Toast.makeText(context, "You Clicked on item no. $newPosition", Toast.LENGTH_SHORT).show()
+                showPopupMenu()
             }
         })
-
-
-
 
         // on below line we are notifying adapter
         // that data has been updated.
         gameRVAdapter.notifyDataSetChanged()
 
-
         return view
+    }
+
+    private fun showPopupMenu() {
+        val popupMenu = PopupMenu(context, view?.findViewById(R.id.idIVGame))
+
+        popupMenu.inflate(R.menu.popup_menu)
+        popupMenu.show()
+
+        popupMenu.setOnMenuItemClickListener(PopupMenu.OnMenuItemClickListener { item: MenuItem ->
+
+            when (item.itemId) {
+
+                R.id.action_item1 -> {
+                    Toast.makeText(context, "action item1 clicked", Toast.LENGTH_SHORT).show()
+                }
+                R.id.action_item2 -> {
+                    Toast.makeText(context, "action item2 clicked", Toast.LENGTH_SHORT).show()
+                }
+                R.id.action_item3 -> {
+                    Toast.makeText(context, "action item3 clicked", Toast.LENGTH_SHORT).show()
+                }
+            }
+            true
+        })
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
